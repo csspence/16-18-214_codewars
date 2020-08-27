6052f8860b3ec6520e6df429666860ac55bd1949
Crew Spence <csspence123@gmail.com>
2020-08-27 06:06:10 -0500

    Created function stub for toy problem.

---
+++ 16+18=214_codewars.js
@@ -0,0 +1,9 @@
+/*
+For this Kata you will have to forget how to add two numbers together.
+
+In simple terms our method does not like the principle of carrying over numbers and just writes down every number it calculates.
+*/
+
+function add(num1, num2) {
+  return num1 + num2;
+}
